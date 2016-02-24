fb3da2c3aab950d33f71a1232f553431219b8ced
danielptm <danielptm@me.com>
2016-02-24 09:19:21 +0100

    Music player on its own thread

---
+++ src/application/musicPlayer/PlayerFunctions.java
@@ -0,0 +1,52 @@
+package application.musicPlayer;
+import java.nio.file.Paths;
+
+import application.view.Main;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class PlayerFunctions implements Runnable{
+	Thread th;
+	String song = Paths.get("//Users/daniel/Documents/javayh/kurs4/kurs4WorkSpace/MusicPlayer/src/application/resources/music/NahkoAndMedicineForThePeople/DarkAsNight/AlohaKeAkuna.mp3").toUri().toString();
+	Media hit;
+	MediaPlayer mp;
+	Main mn;
+	public static boolean running=false;
+	
+	public static boolean isRunning() {
+		return running;
+	}
+
+	public static void setRunning(boolean trueOrFalse) {
+		running = trueOrFalse;
+	}
+
+	public PlayerFunctions(){
+		th = new Thread(this);
+		hit = new Media(song);
+		mp = new MediaPlayer(hit);
+	}
+	
+	public void startSong(){
+		setRunning(true);
+		th.start();
+	}
+	
+	public void pauseSong(){
+		setRunning(false);
+		mp.pause();
+		try {
+			th.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while(running){
+			mp.play();
+		}
+	}
+}
---
+++ src/application/view/Main.java
@@ -0,0 +1,70 @@
+package application.view;
+
+import application.musicPlayer.PlayerFunctions;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
+public class Main extends Application{
+
+	@FXML Button playBtn;
+	@FXML ImageView btnImg;
+	PlayerFunctions pf;
+	
+	@FXML
+	public void initialize(){
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(Main.class.getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			scene.setOnKeyPressed((KeyEvent)->keyBoardHandler(KeyEvent) );
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@FXML
+	public void keyBoardHandler(KeyEvent ke){
+		if(ke.getCode()==ke.getCode().S){
+			if(PlayerFunctions.isRunning()){
+				stopSongThread();
+			}
+			else{
+				//stopSongThread();
+				startSongThread();
+				}
+		}
+	}
+	
+	@FXML
+	public void startSongThread(){
+		if(PlayerFunctions.isRunning()==false){
+			pf = new PlayerFunctions();
+			pf.startSong();
+		}
+	}
+	
+	@FXML
+	public void stopSongThread(){
+		if(PlayerFunctions.isRunning()==true){
+			pf.pauseSong();
+		}
+	}
+}
